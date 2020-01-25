8e19abae25bca458d35237a6c0765240a024f1e8
Karan Chawla <karan.chawla.19@ucl.ac.uk>
2020-01-25 00:13:15 +0000

    final add

---
+++ lib/programcounter32.v
@@ -0,0 +1,18 @@
+module programcounter32new (
+	input         CLK, 
+	input         RST,
+	input  [31:0] D, 
+	output [31:0] Q);
+
+  reg [31:0] r;
+
+  always @(posedge CLK or posedge RST)
+	if (RST == 1)
+		r = 0;
+	else
+		r = D;	
+
+  assign Q = r;
+
+
+endmodule
